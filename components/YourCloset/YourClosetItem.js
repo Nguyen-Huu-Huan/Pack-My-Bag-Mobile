8911b898d5a2e1926fdbf0045d03991bd965b644
Huan Nguyen <s3887111@rmit.edu.vn>
2021-12-28 13:08:46 +0700

    minor fixes

--- components/YourCloset/YourClosetItem.js
+++ components/YourCloset/YourClosetItem.js
@@ -1,54 +1,39 @@
 import React from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  Avatar,
-  ScrollView,
-  StyleSheet,
-  Image,
-} from "react-native";
+import { View, Text, TouchableOpacity, Avatar, ScrollView, StyleSheet, Image } from "react-native";
 import { Icon } from "react-native-elements/dist/icons/Icon";
 import { ListItem, Button } from "react-native-elements";
 const YourClosetItem = ({ navigation, item_data, item_index, remove_item }) => {
-  var image_source = item_data.icon;
-  return (
-    <View style={{ marginBottom: 10, background: "blue" }}>
-      <ListItem.Swipeable
-        bottomDivider
-        leftContent={
-          <Button
-            title="Details"
-            icon={{ name: "details", color: "white" }}
-            buttonStyle={{ minHeight: "100%", backgroundColor: "red" }}
-            onPress={() =>
-              navigation.navigate("ItemInfo", { item_data: [item_data] })
-            }
-          />
-        }
-        rightContent={
-          <Button
-            title="Remove"
-            icon={{ name: "delete", color: "white" }}
-            buttonStyle={{ minHeight: "100%", backgroundColor: "red" }}
-            onPress={() => {
-              remove_item(item_index);
-            }}
-          />
-        }
-      >
-        <Image style={styles.image} source={{ uri: image_source }} />
-        <ListItem.Content>
-          <ListItem.Title>{item_data.name}</ListItem.Title>
-        </ListItem.Content>
-      </ListItem.Swipeable>
-    </View>
-  );
+	var image_source = item_data.icon;
+	return (
+		<View style={{ marginBottom: 10, background: "blue" }}>
+			<ListItem.Swipeable
+				bottomDivider
+				leftContent={
+					<Button title="Details" icon={{ name: "details", color: "white" }} buttonStyle={{ minHeight: "100%", backgroundColor: "red" }} onPress={() => navigation.navigate("ItemInfo", { item_data: [item_data], view_info: true })} />
+				}
+				rightContent={
+					<Button
+						title="Remove"
+						icon={{ name: "delete", color: "white" }}
+						buttonStyle={{ minHeight: "100%", backgroundColor: "red" }}
+						onPress={() => {
+							remove_item(item_index);
+						}}
+					/>
+				}
+			>
+				<Image style={styles.image} source={{ uri: image_source }} />
+				<ListItem.Content>
+					<ListItem.Title>{item_data.name}</ListItem.Title>
+				</ListItem.Content>
+			</ListItem.Swipeable>
+		</View>
+	);
 };
 export default YourClosetItem;
 const styles = StyleSheet.create({
-  image: {
-    width: 50,
-    height: 50,
-  },
+	image: {
+		width: 50,
+		height: 50,
+	},
 });
